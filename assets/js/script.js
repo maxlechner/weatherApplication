501af9afa9af4f36180d8463e4d1dae15c0a8cea
Max Lechner <mkonlech@gmail.com>
2021-03-21 16:51:53 -0700

    updated ajax calls for forecast, uv. Added in save function for local storage of city searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
-
 // define my variables
+
+let cityList = [];
+let now = moment().format("dddd, MMMM Do, YYYY"); 
+const apiKey = "fbc8e0f7f4930b8cc94ef9a73ca2f05d";
+
 
 // WHEN I click the search button
 
-var cityName;
+$("#add-city").on("click", function ( event ) {
 
-function handleSearch() {
+    event.preventDefault();
 
-    makeWeatherRequest( search );
+    let city = $("#city-search").val();
 
-}
+    console.log(city)
+
+    const getCitySearches = localStorage.getItem("citySearches");
+
+    let prevSearches = [];
+    const formatSearches = JSON.parse(getCitySearches)
+
+    if (formatSearches) {
+        prevSearches = formatSearches;
+    }
+
+    // adds the current city search to the previous search array
+    prevSearches.push( city );
+
+    const searchHistory = JSON.stringify(prevSearches);
+    localStorage.setItem("searchHistory", searchHistory);
+
+    citySearch( city );
+
+})
 
 // function makeWeatherRequest( search ) {
 // function makeWeatherRequest( ) {
     // Perfoming an AJAX GET request to our queryURL
 
 
-$( document ).ready(function() {
+function citySearch ( city ) {
 
-    var now = moment().format("dddd, MMMM Do, YYYY");
-
-    $('#currentDay').text(now);
-
-    var apiKey = "fbc8e0f7f4930b8cc94ef9a73ca2f05d";
-
-    $(".query_btn").click(function(){
-
-        var query_param = $(this).prev().val();
-
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + apiKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
 
         $.ajax({
             url: queryURL,
@@ -54,9 +54,30 @@ $( document ).ready(function() {
             $("#temperature").text(((((response.main.temp)-273.15)*9/5)+32).toFixed(2));
             $("#pressure").text(response.main.pressure);
             $("#humidity").text(response.main.humidity);
-        });
-    })
 
-});
+            let long = response.coord.lon;
+            let lat = response.coord.lat;
 
-// $("#find-city").on("click", makeWeatherRequest);
+            var uvQueryUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&APPID=" + apiKey;
+
+            $.ajax({
+                url: uvQueryUrl,
+                method: "GET"
+            }).then(function(uvGet) {
+                console.log(uvGet)
+                $("#uvindex").text(uvGet.value);
+            });
+
+            var forecastQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=" + apiKey;
+
+            $.ajax({
+                url: forecastQueryUrl,
+                method: "GET"
+            }).then(function(forecast) {
+                console.log(forecast)
+                // $("#uvindex").text(uvGet.value);
+            });
+
+
+    });
+}
